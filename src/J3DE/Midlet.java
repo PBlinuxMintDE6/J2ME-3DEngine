4eb6f076ed1d8d8e7bee1256547a68e84729845d
PBlinuxMintDE6 <>
2026-01-10 19:47:15 +0000

    Renaming of project from the default

--- src/J3DE/Midlet.java
+++ src/J3DE/Midlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mobileapplication1;
+package J3DE;
 
 import javax.microedition.midlet.*;
 import javax.microedition.lcdui.*;
--- src/J3DE/Polygon.java
+++ src/J3DE/Polygon.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package mobileapplication1;
+package J3DE;
 
 import java.util.Vector;
 
